9be89d9004b81e9dd7867a94b71d7e3830a1a381
Elaidzha1940 <eli080293@gmail.com>
2023-05-24 19:41:38 +0700

    Test 24

---
+++ Test 24/Test 24/Test_24App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_24App.swift
+//  Test 24
+//
+//  Created by Elaidzha Shchukin on 24.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Test_24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
